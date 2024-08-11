a0e04be9aa29a22a2d7d67e26dcecc02e321f8f1
aliaabdalrhman <aliaabdalrhman2001@gmail.com>
2024-08-11 20:40:00 +0300

    Create Authentication [login,register]

--- Src/Modules/Auth/Auth.controller.js
+++ Src/Modules/Auth/Auth.controller.js
@@ -1,5 +1,39 @@
+import userModel from "../../../DB/Models/User.model.js";
+import bcrypt from 'bcryptjs';
+import jwt from "jsonwebtoken";
 
-export const register = (req, res) => {
+export const register = async (req, res) => {
+    try {
+        const { userName, email, password } = req.body;
+        const user = await userModel.findOne({ email });
+        if (user) {
+            return res.status(409).json({ message: "Email already exists" });
+        }
+        else {
+            const hashedPassword = bcrypt.hashSync(password, parseInt(process.env.SALTROUND));
+            await userModel.create({ userName, email, password: hashedPassword });
+            return res.status(201).json({ message: "success" });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Server Error", error: error.stack });
+    }
 
-    return res.json('register');
+}
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+        return res.status(404).json({ message: "user not found" });
+    }
+    else {
+        const isMatch = bcrypt.compareSync(password, user.password);
+        if (!isMatch) {
+            return res.status(404).json({ message: "invalid password" });
+        }
+        else {
+            const token = jwt.sign({ id: user._id }, process.env.LOGINSIGNATURE, { expiresIn: "1h" });
+            return res.status(200).json({ message: "success", token });
+        }
+    }
 }
--- Src/Modules/Auth/Auth.router.js
+++ Src/Modules/Auth/Auth.router.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import * as authController from './Auth.controller.js'
 const router = Router();
+
 router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 export default router;
