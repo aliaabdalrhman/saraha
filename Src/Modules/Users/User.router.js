dd718edbd787e6da21980d567b1c01b7e4738f5d
aliaabdalrhman <aliaabdalrhman2001@gmail.com>
2024-08-20 17:56:21 +0300

    Update User.router

--- Src/Modules/Users/User.router.js
+++ Src/Modules/Users/User.router.js
@@ -1,8 +1,6 @@
 import { Router } from "express";
 import * as UserController from './User.controller.js'
-import { auth } from "../../Middelware/Auth.js";
 const router = Router();
-
 
 router.get('/',UserController.getAllUsers);
 
